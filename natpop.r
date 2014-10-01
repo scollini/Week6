03d738bcfd0ac96aa4eb820166814b3178b5cea2
Sara Collini <scolli16@masonlive.gmu.edu>
2014-09-30 22:41:48 -0400

    Added r script comments

--- natpop.r
+++ natpop.r
@@ -9,7 +9,7 @@ library(lubridate)
 state <- read.csv("~/Desktop/Clio-3/Week6/historydata/data-raw/nhgis0011_ts_state.csv", stringsAsFactors = FALSE)
 
 nat_pop <- state %>% ## Create a new name for the table
-  group_by(YEAR) %>% ## Group each different census year together
+  group_by(YEAR) %>% ## Group each different census year together from the state csv 
   summarize(Population = sum(A00AA, na.rm = TRUE)) ## Then sum up each state population for that census
 
 
